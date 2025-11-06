034a62b96ee1e5c53beae0eab1138d5584820fba
HarshaBandla <harshacodex@gmail.com>
2025-11-05 20:17:35 -0600

    Use database logic instead of csv

--- web_scraper_dashboard/extract_jobs.py
+++ web_scraper_dashboard/extract_jobs.py
@@ -1,6 +1,5 @@
 from web_scraper_dashboard.constants import company_field, title_field, link_field, company_header, job_header, link_header, main_url
 
-
 def extract_jobs(job_listing):
     company_name = job_listing.find('p', class_=company_field).text.strip()
     title = job_listing.find('h3', class_=title_field).text.strip()
--- web_scraper_dashboard/main.py
+++ web_scraper_dashboard/main.py
@@ -1,19 +1,25 @@
 from constants import url, job_listing_field, file
-from csv_handler import write_to_csv
 from extract_jobs import extract_jobs
 from web_scraper import scraper
 from bs4 import BeautifulSoup
 
+from web_scraper_dashboard.db_connection_helper import connect_to_db, save_jobs_to_db, get_jobs, delete_jobs
+
 def main():
+
+    connect_to_db()
+
+    delete_jobs()
+
     response = scraper(url)
     soup = BeautifulSoup(response, 'html.parser')
     job_listings = soup.find_all('li', class_=job_listing_field)
 
     jobs = map(lambda job: extract_jobs(job), job_listings)
 
-    write_to_csv(jobs, file)
+    save_jobs_to_db(jobs)
 
-    print(f"Saved data to file:{file}")
+    get_jobs()
 
 if __name__=="__main__":
     main()
